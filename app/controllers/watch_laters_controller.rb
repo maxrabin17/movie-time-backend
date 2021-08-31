64b8ef0e3a1e517f77112d9c539337f9b4ba2329
maxrabin <max.rabin17@gmail.com>
2021-08-31 15:28:12 -0400

    added destroy action to watchlater controller

--- app/controllers/watch_laters_controller.rb
+++ app/controllers/watch_laters_controller.rb
@@ -1,2 +1,29 @@
 class WatchLatersController < ApplicationController
+
+
+    def create
+        # byebug
+        movie = WatchLater.create(watch_later_params)
+        # if movie.save!
+            render json: movie, status: :created
+        # end
+    end
+
+    def destroy
+        if session[:user_id]
+            watch_later = find_watch_later
+            watch_later.destroy
+            head :no_content
+        end
+    end
+
+    private
+
+    def watch_later_params
+        params.permit(:title, :poster, :user_id)
+    end
+
+    def find_watch_later
+        WatchLater.find(params[:id])
+    end
 end
--- app/models/watch_later.rb
+++ app/models/watch_later.rb
@@ -1,2 +1,3 @@
 class WatchLater < ApplicationRecord
+    belongs_to :user
 end
